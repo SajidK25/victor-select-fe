3fa007c1f843915825da9929e504bc269d669fe0
Brian Baker <bakerman59@gmail.com>
2020-05-19 10:59:25 -0700

    Server config  in .env variable

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
-export const dev_endpoint = `http://localhost:4444/`;
-export const prod_endpoint = "https://victory-select-server.herokuapp.com/";
+export const dev_endpoint = process.env.REACT_APP_API_ENDPOINT;
+//`http://localhost:4444/`;
+export const prod_endpoint = process.env.REACT_APP_API_PRODENDPOINT;
+// "https://victory-select-server.herokuapp.com/";
 export const perPage = 4;
