82fe53112498f8fd0e3d215c3d97fc8b8366c937
Brian Baker <bakerman59@gmail.com>
2020-09-09 13:56:23 -0700

    Testing

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 
-// export const GA_ANALYTICS = "";
-export const GA_ANALYTICS = "UA-162995465-1";
+export const GA_ANALYTICS = "";
+//export const GA_ANALYTICS = "UA-162995465-1";
 
-export const endpoint = "http://localhost:4444/";
+//export const endpoint = "http://localhost:4444/";
 //export const endpoint = "https://victory-select-server.herokuapp.com/";
-// export const endpoint = "https://vs-testserver.herokuapp.com/";
+export const endpoint = "https://vs-testserver.herokuapp.com/";
 
 export const perPage = 4;
